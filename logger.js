2703d06884c5eced838d595cf7902b4a33d5a390
Mostafa Nagueb <mostafanagueb@gmail.com>
2025-12-20 18:06:02 +0300

    configure for prod deployment

--- logger.js
+++ logger.js
@@ -1,21 +1,26 @@
-const env = require("./config/default.json").env;
+const config = require("config");
+const env = config.get("env");
 const { createLogger, transports } = require("winston");
 const logform = require("logform");
 const { combine, timestamp, label, printf } = logform.format;
 
 module.exports.intialize = () => {
-	let prod = env === "production" ? true : false;
-	module.exports.logger = createLogger({
-		format: combine(
-			label({ label: `${env}` }),
-			timestamp(),
-			printf((nfo) => {
-				return `${nfo.timestamp} [${nfo.label}] ${nfo.level}: ${nfo.message} </br>`;
-			})
-		),
-		transports: [
-			new transports.Console(),
-			new transports.File(prod === true ? { filename: "./logs/production.log" } : {filename: "./logs/development.log"}),
-		],
-	});
+  let prod = env === "production" ? true : false;
+  module.exports.logger = createLogger({
+    format: combine(
+      label({ label: `${env}` }),
+      timestamp(),
+      printf((nfo) => {
+        return `${nfo.timestamp} [${nfo.label}] ${nfo.level}: ${nfo.message} </br>`;
+      })
+    ),
+    transports: [
+      new transports.Console(),
+      new transports.File(
+        prod === true
+          ? { filename: "./logs/production.log" }
+          : { filename: "./logs/development.log" }
+      ),
+    ],
+  });
 };
