97250f01afd103c190a8f6c6fc5ec62b36286027
Mostafa Nagueb <mostafanagueb@gmail.com>
2026-01-11 19:58:01 +0300

    adding new endpoint for get single course, edit update endpoint to use id rather than course name

--- src/restAPI/routes/route_course.js
+++ src/restAPI/routes/route_course.js
@@ -5,28 +5,39 @@ const router = express.Router();
 const controller_appraisal_modules = require("../controllers/controller_course");
 
 /**
- *      /create used to create a new course 
+ *      /create used to create a new course
  */
 router.put("/create", controller_appraisal_modules.courseCreateAPI);
 
 /**
- *      /update used to update a specific course 
- */
-router.post("/update", controller_appraisal_modules.courseUpdateAPI);
-
-/**
- *      /create used to create a new course 
+ *      /create used to create a new course
  */
 router.post("/register", controller_appraisal_modules.courseRegisterAPI);
 
 /**
  *      /get-available-courses used to fetch all available courses
  */
-router.get("/get-available-courses", controller_appraisal_modules.getAvailableCoursesAPI);
+router.get(
+  "/get-available-courses",
+  controller_appraisal_modules.getAvailableCoursesAPI
+);
 
 /**
  *      /get-available-courses used to fetch all available courses
  */
-router.get("/get-registered-courses", controller_appraisal_modules.getRegisteredCoursesAPI);
+router.get(
+  "/get-registered-courses",
+  controller_appraisal_modules.getRegisteredCoursesAPI
+);
+
+/**
+ *      /read used to fetch a specific course
+ */
+router.get("/:id", controller_appraisal_modules.courseReadAPI);
+
+/**
+ *      /update used to update a specific course
+ */
+router.put("/update/:id", controller_appraisal_modules.courseUpdateAPI);
 
 module.exports = router;
